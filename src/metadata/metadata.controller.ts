e27c9bfcc2a4ffae17501f4dd06c656f5009209c
Darren Meiring <dpmeiring@gmail.com>
2025-06-23 15:56:25 +0800

    Made Metadata Controller Private

--- src/metadata/metadata.controller.ts
+++ src/metadata/metadata.controller.ts
@@ -2,7 +2,6 @@ import { Controller, Get, Param } from '@nestjs/common';
 import { MetadataService } from './metadata.service';
 import { Public } from 'src/auth/functions';
 
-@Public()
 @Controller('metadata')
 export class MetadataController {
   constructor(private readonly metadataService: MetadataService) {}
